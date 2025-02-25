ff21a632ed0feca03ea997362d82fe90bd3adef6
EdiMena <edison14_93@hotmail.com>
2025-02-25 00:32:42 -0500

    feat: broker changes done, console erros, life cicle angular.

--- src/app/modules/personal-profile/broker/broker-details/broker-details.component.ts
+++ src/app/modules/personal-profile/broker/broker-details/broker-details.component.ts
@@ -2,12 +2,13 @@ import {
   AfterViewInit,
   ChangeDetectorRef,
   Component,
-  OnInit,
+  OnInit, ViewChild,
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BrokerService } from '../../services/broker.service';
-import { IReadBrokerModel } from '../../interfaces/broker.interface'; // Make sure this path is correct
-import { Observable } from 'rxjs';
+import {IReadBrokerModel, IUpdateBrokerModel} from '../../interfaces/broker.interface';
+import {NgForm} from "@angular/forms";
+import Swal, {SweetAlertOptions} from "sweetalert2";
 
 type Tabs = 'Details' | 'Payment Info';
 
@@ -17,10 +18,33 @@ type Tabs = 'Details' | 'Payment Info';
   styleUrls: ['./broker-details.component.scss'],
 })
 export class BrokerDetailsComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('brokerForm') brokerForm!: NgForm;
   isCollapsed: boolean = false;
-  brokerData!: IReadBrokerModel | null; // Broker details object
+  brokerData: IReadBrokerModel = {
+    id: '',
+    deletedAt: '',
+    buyerItBelongs: { id: '', fullName: '' },
+    person: {
+      id: '',
+      names: '',
+      lastNames: '',
+      identification: '',
+      birthDate: new Date(),
+      address: '',
+      phone: '',
+      mobilePhone: '',
+      mobilePhone2: '',
+      email: '',
+      emergencyContactName: '',
+      emergencyContactPhone: ''
+    }
+  };
+  // Broker details object
   personId: string;
   isLoading: boolean = true; // Added loading state
+  formattedBirthDate: string = '';
+
 
   activeTab: Tabs = 'Details';
 
@@ -45,12 +69,14 @@ export class BrokerDetailsComponent implements OnInit, AfterViewInit {
       next: (broker) => {
         this.brokerData = broker;
         this.personId = broker.person!.id; // Extract personId from broker data
+        if (broker.person.birthDate) {
+          this.formattedBirthDate = new Date(broker.person.birthDate).toISOString().split('T')[0];
+        }
         this.isLoading = false;
         this.changeDetectorRef.detectChanges(); // Ensure UI updates
       },
       error: (err) => {
         console.error('Error fetching broker details:', err);
-        this.brokerData = null;
         this.isLoading = false;
       },
     });
@@ -58,6 +84,62 @@ export class BrokerDetailsComponent implements OnInit, AfterViewInit {
 
   setActiveTab(tab: Tabs) {
     this.activeTab = tab;
+  }
+
+  saveBroker(){
+    if (this.brokerForm.invalid || !this.brokerData) {
+      return;
+    }
+    this.isLoading = true;
+
+    const payload: IUpdateBrokerModel = {
+      id: this.brokerData.id,
+      deletedAt: this.brokerData.deletedAt,
+      buyerItBelongs: this.brokerData.buyerItBelongs.id,
+      person: this.brokerData.person,
+    }
+
+    this.brokerService.updateBroker(this.brokerData.id, payload).subscribe({
+      next: ()=> {
+        this.isLoading = false;
+        this.showSuccessAlert()
+      },
+      error: (error) => {
+        this.isLoading = false;
+        console.error('Error updating broker', error);
+        this.showErrorAlert(error);
+      }
+    });
+  }
+
+  get brokerPerson() {
+    return this.brokerData.person!;
+  }
+
+  private showSuccessAlert() {
+    const options: SweetAlertOptions = {
+      title: '¡Éxito!',
+      text: 'Los cambios se guardaron correctamente',
+      icon: 'success',
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#3085d6',
+      timer: 5000,
+      timerProgressBar: true,
+    };
+    Swal.fire(options);
+  }
+
+  private showErrorAlert(error: any) {
+    const options: SweetAlertOptions = {
+      title: 'Error',
+      html: `<strong>${error.message || 'Ocurrió un error inesperado.'}</strong>`,
+      icon: 'error',
+      confirmButtonText: 'Entendido',
+      confirmButtonColor: '#d33',
+      showCloseButton: true,
+      focusConfirm: false,
+    };
+    Swal.fire(options);
   }
 
   ngAfterViewInit(): void {}
