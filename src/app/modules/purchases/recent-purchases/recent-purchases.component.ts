3a0cf967697f9a90de251a13a4d7c00daa9f4698
Nicolas Morillo <nicolas.morillo@terradotta.com>
2025-03-18 22:59:31 -0500

    update route for RecentPurchasesComponent

---
+++ src/app/modules/purchases/recent-purchases/recent-purchases.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-purchases',
+  templateUrl: './recent-purchases.component.html',
+  styleUrl: './recent-purchases.component.scss'
+})
+export class RecentPurchasesComponent {
+
+}
