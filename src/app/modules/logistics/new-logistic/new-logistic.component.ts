a41fc3dc390d040f8d636c2183317bd087711d1f
Nicolas Morillo <nicolas.morillo@terradotta.com>
2025-03-26 02:45:03 -0500

    ok load purchase data

--- src/app/modules/logistics/new-logistic/new-logistic.component.ts
+++ src/app/modules/logistics/new-logistic/new-logistic.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-import {Observable} from "rxjs";
+import {ChangeDetectorRef, Component, EventEmitter} from '@angular/core';
+import {Observable, Subscription} from "rxjs";
 import {NgForm} from "@angular/forms";
 import {DateUtilsService} from "../../../utils/date-utils.service";
 import {ICreateLogisticModel} from "../interfaces/logistic.interface";
+import {AuthService} from "../../auth";
+import {IListPurchaseModel} from "../../purchases/interfaces/purchase.interface";
+import {PurchaseService} from "../../purchases/services/purchase.service";
 
 @Component({
   selector: 'app-new-logistic',
@@ -10,13 +13,27 @@ import {ICreateLogisticModel} from "../interfaces/logistic.interface";
   styleUrl: './new-logistic.component.scss'
 })
 export class NewLogisticComponent {
+  private unsubscribe: Subscription[] = [];
 
   isLoading$: Observable<boolean>;
+  isOnlyBuyer = false;
+  reloadEvent: EventEmitter<boolean> = new EventEmitter();
+  controlNumber: number = 0;
+  buyer: string = '';
+  broker: string = '';
+  company: string = '';
+  shrimpFarm: string = '';
+  farmPlace: string = '';
+  purchaseDate: string = '';
 
+  controlNumberPurchase: IListPurchaseModel = {} as IListPurchaseModel;
   createLogisticModel: ICreateLogisticModel = {} as ICreateLogisticModel;
 
   constructor(
+    private authService: AuthService,
+    private cdr: ChangeDetectorRef,
     private dateUtils: DateUtilsService,
+    private purchaseService: PurchaseService,
   ) {  }
 
   get logisticDateFormatted(): string | null {
@@ -31,6 +48,37 @@ export class NewLogisticComponent {
 
   onDateChange(event: any): void {
     // date change logic
+  }
+
+  searchPurchase(): void {
+    console.log('search purchase');
+    console.log('controlNumber:', this.controlNumber);
+
+    const userId: string | null = this.isOnlyBuyer
+      ? this.authService.currentUserValue?.id ?? null
+      : null;
+
+    const purchaseSub = this.purchaseService
+      .getPurchaseByParams(false, userId, null, this.controlNumber)
+      .subscribe({
+        next: (purchases: IListPurchaseModel[]) => {
+          console.log(purchases);
+          console.log('controlNumberPurchase:', this.controlNumberPurchase);
+          this.controlNumberPurchase = purchases[0];
+          this.buyer = this.controlNumberPurchase.buyer;
+          this.broker = this.controlNumberPurchase.broker;
+          this.company = this.controlNumberPurchase.company;
+          this.shrimpFarm = this.controlNumberPurchase.shrimpFarm;
+          this.farmPlace = this.controlNumberPurchase.shrimpFarm;
+          this.purchaseDate = this.dateUtils.formatISOToDateInput(this.controlNumberPurchase.purchaseDate);
+          this.reloadEvent.emit(true);
+          this.cdr.detectChanges();
+        },
+        error: (error) => {
+          console.error('Error fetching purchases:', error);
+        },
+      });
+    this.unsubscribe.push(purchaseSub);
   }
 
 }
